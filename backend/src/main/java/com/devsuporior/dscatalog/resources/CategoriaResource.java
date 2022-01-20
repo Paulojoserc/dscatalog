7764f725ba5a8e4b3d101b38c7267bfeaf366d57
Paulo Costa <pjrc83@gmail.com>
2022-01-20 11:36:47 -0300

    DTO

--- backend/src/main/java/com/devsuporior/dscatalog/resources/CategoriaResource.java
+++ backend/src/main/java/com/devsuporior/dscatalog/resources/CategoriaResource.java
@@ -9,19 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuporior.dscatalog.dto.CategoryDTO;
 import com.devsuporior.dscatalog.entities.Category;
 import com.devsuporior.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriaResource {
-	/*Autowired injeta automaticamente as dependencias */
+	/* Autowired injeta automaticamente as dependencias */
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/devsuporior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuporior/dscatalog/services/CategoryService.java
@@ -1,24 +1,35 @@
 package com.devsuporior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuporior.dscatalog.dto.CategoryDTO;
 import com.devsuporior.dscatalog.entities.Category;
 import com.devsuporior.dscatalog.repositories.CategoryRepository;
+
 /*@Service Ela vai registrar a class como um componente que vai participa do sistema de injeção do sistema de 
 de gestão de independência automatizado do spring quer dizer quem vai gerenciar as instancias das  
-dependências  dos objetos categoria service vai ser o spring */ 
-@Service 
+dependências  dos objetos categoria service vai ser o spring */
+@Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	//@Transactional serve para fazer trnsação com banco de dados
+
+	// @Transactional serve para fazer trnsação com banco de dados
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		/*
+		 * Modo verboso List<CategoryDTO> listDto = new ArrayList<>(); for (Category cat
+		 * : list) { listDto.add(new CategoryDTO(cat));
+		 */
 	}
+	// return listDto;
 }
