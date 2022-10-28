e7df15d5297a5e1ced13c37da71afdb0cfb6956c
Paulo Costa <pjrc83@gmail.com>
2022-10-28 17:28:02 -0300

    Testando outro cenário do delete

--- backend/src/test/java/com/devsuporior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuporior/dscatalog/repositories/ProductRepositoryTests.java
@@ -6,21 +6,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuporior.dscatalog.entities.Product;
 
 @SpringBootTest
 public class ProductRepositoryTests {
-	
+
 	@Autowired
 	private ProductRepository repository;
+
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		
+
 		long exintingId = 1L;
 		repository.deleteById(exintingId);
-		
+
 		Optional<Product> result = repository.findById(exintingId);
 		Assertions.assertFalse(result.isPresent());
+	}
+
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+
+		long nonExistingId =1000L;
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
+		
 	}
 }
