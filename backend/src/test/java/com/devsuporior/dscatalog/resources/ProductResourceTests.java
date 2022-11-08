036e8af1178e6f42300f30f335f99cd790547fe6
Paulo Costa <pjrc83@gmail.com>
2022-11-08 14:37:56 -0300

    Simulando outros comportamentos do ProductService

--- backend/src/test/java/com/devsuporior/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/com/devsuporior/dscatalog/resources/ProductResourceTests.java
@@ -4,6 +4,8 @@ package com.devsuporior.dscatalog.resources;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -26,6 +28,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import com.devsuporior.dscatalog.dto.ProductDTO;
 import com.devsuporior.dscatalog.repositories.tests.Factory;
 import com.devsuporior.dscatalog.services.ProductService;
+import com.devsuporior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuporior.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -42,6 +45,7 @@ public class ProductResourceTests {
 
 	private Long existingId;
 	private Long nonExistingId;
+	private Long dependentId;
 	private ProductDTO productDTO;
 	private PageImpl<ProductDTO> page;
 
@@ -50,6 +54,7 @@ public class ProductResourceTests {
 
 		existingId = 1L;
 		nonExistingId = 2L;
+		dependentId = 3L;
 
 		productDTO = Factory.createProductDTO();
 		page = new PageImpl<>(List.of(productDTO));
@@ -61,6 +66,10 @@ public class ProductResourceTests {
 
 		when(service.update(eq(existingId), any())).thenReturn(productDTO);
 		when(service.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+		
+		doNothing().when(service).delete(existingId);
+		doThrow(ResourceNotFoundException.class).when(service).delete(nonExistingId);
+		doThrow(DatabaseException.class).when(service).delete(dependentId);
 	}
 
 	@Test
